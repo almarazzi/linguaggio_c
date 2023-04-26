5ecf89b86726448934adcbbe604b65d2055286bf
almarazzi <almarazzi05@gmail.com>
2023-04-26 21:47:36 +0200

    caricamento programma in c

--- 9c_marazzi_alberto/9c_marazzi_alberto.c
+++ 9c_marazzi_alberto/9c_marazzi_alberto.c
@@ -6,37 +6,27 @@
 
 void main() 
 {
-	int num=0;
+	int num=0,i=2;
 	printf("inserisi un numero: ");
 	scanf("%d",&num);
-	int lista[num-2];
-	int risultato[num-2];
-	int y = 2;
-	int mol = 0;
-	int i=0,g=0,u=0;
-	for (i = 0; i < sizeof(lista) / sizeof(lista[0]); i++)
+	while(i<num)
 	{
-		lista[i] = y;
-    	risultato[i] = y;
-    	y++;
-	}
-	for (g = 0; g < sizeof(lista) / sizeof(lista[0]); g++)
-	{
-    	for (u = 0; u < sizeof(lista) / sizeof(lista[0]); u++)
-   		{
-        	mol = lista[g] * lista[u];
- 			for (i = 0; i < sizeof(risultato) / sizeof(risultato[0]); i++)
+		num--;
+		int u=2,trorfa=0;
+		while(u<num)
+		{
+			if(num%u==0)
 			{
-				if (mol == risultato[i])
- 				{
-		   		    risultato[i] = -1;
-				}
+				trorfa=1;
+				u=num+1;
 			}
-        }
-    }
-	for (i = 0; i < sizeof(risultato) / sizeof(risultato[0]); i++)
-	{
-    	printf("i numeri primi sono =>%d\n",risultato[i]);
+			u++;
+		}
+		if(trorfa==0)
+		{
+			printf("%d e un num primo\n",num);
+		}
+		
 	}
 	system("pause");
 }
